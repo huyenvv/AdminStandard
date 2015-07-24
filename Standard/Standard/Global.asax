d635bd2076e05be6c20f13a2b7e4eba795c4666d
huyenvv <huyenvv.it@gmail.com>
2015-07-25 01:48:00 +0700

    Create new

---
+++ Standard/Standard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Standard.MvcApplication" Language="C#" %>
---
+++ Standard/Standard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Standard.Startup))]
+namespace Standard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
