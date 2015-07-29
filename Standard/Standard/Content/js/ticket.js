33eaec39e0d70bdd19e0d15b88d10bd123ca969f
huyenvv <huyenvv.it@gmail.com>
2015-07-30 01:12:32 +0700

    fgghh

---
+++ Standard/Standard/Content/js/ticket.js
@@ -0,0 +1,57 @@
+﻿$(function () {
+    $('#ThemChiTiet').click(function () {
+
+        var check = true;
+        var soluong = $("#Quantity").val();
+        $("input, textarea", "#frmChiTietYeuCau").each(function myfunction() {
+            var value = $(this).val();
+            if (value == "" || (this.id == "Quantity" && parseInt(value) <= 0)) {
+                check = false;
+                $(this).addClass("input-error");
+            } else {
+                $(this).removeClass("input-error");
+            }
+        });
+        if (check) {
+            $("#messerror").addClass("hide");
+            var no = $("table tbody tr").length + 1;
+            var dienGiai = $("#DienGiai").val();
+            var mucDich = $("#MucDich").val();
+            var ngayCan = $("#NgayCan").val();
+            var data = {
+                Id: no,
+                DienGiai: dienGiai,
+                SoLuong: soluong,
+                LyDo: mucDich,
+                NgayCan: ngayCan
+            };
+            $.post("/Ticket/AddTicketDetail", data)
+              .done(function (ok) {
+                  $("table tbody").append("<tr><td>" + no + "</td><td>" + dienGiai + "</td><td>" + soluong + "</td><td>" + mucDich + "</td><td>" + ngayCan + "</td></tr>");
+              });
+        } else {
+            $("#messerror").removeClass("hide");
+        }
+        return false;
+    });
+
+
+
+    $('input.inlineCheckbox1').on('change', function () {
+        $('input.inlineCheckbox1').not(this).prop('checked', false);
+    });
+});
+
+function CheckTicket() {
+    if ($('input.inlineCheckbox1:checked').length > 0) {
+        $("#loaiticket").addClass("hide");
+        if ($("table tbody tr").length > 0) {
+            $("#loaiticketDetail").addClass("hide");
+            return true;
+        }
+        $("#loaiticketDetail").removeClass("hide");
+    } else {
+        $("#loaiticket").removeClass("hide");
+    }
+    return false;
+}
--- Standard/Standard/Controllers/TicketController.cs
+++ Standard/Standard/Controllers/TicketController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Standard.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebLib;
+using WebLib.DAL;
 
 namespace Standard.Controllers
 {
@@ -29,15 +32,74 @@ namespace Standard.Controllers
             return View();
         }
 
+        public JsonResult AddTicketDetail(TicketDetail detail)
+        {
+            var listTicketDetail = new List<TicketDetail>();
+            if (SessionUtilities.Exist(SESSION.TicketDetail))
+            {
+                listTicketDetail = (List<TicketDetail>)SessionUtilities.Get(SESSION.TicketDetail);
+            }
+            listTicketDetail.Add(detail);
+            SessionUtilities.Set(SESSION.TicketDetail, listTicketDetail);
+            return Json(new { }, JsonRequestBehavior.AllowGet);
+        }
+
         //
         // POST: /Ticket/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(int type, int deptId)
         {
             try
             {
                 // TODO: Add insert logic here
+                if (type > 0 && deptId > 0)
+                {
+                    if (SessionUtilities.Exist(SESSION.TicketDetail))
+                    {
+                        var db = DB.Entites;
+                        var listTicketDetail = (List<TicketDetail>)SessionUtilities.Get(SESSION.TicketDetail);
+                        if (listTicketDetail.Count > 0)
+                        {
+                            var currentUser = new fwUserDAL().GetByUserName(DB.CurrentUser.Identity.Name);
+                            if (currentUser != null)
+                            {
+                                // Create ticket
+                                var ticket = new Ticket
+                                {
+                                    Current = currentUser.ID,
+                                    Created = DateTime.Now,
+                                    CreatedBy = currentUser.ID,
+                                    Status = (int)Status.ChoDuyet,
+                                    Track = currentUser.ID + "#;",
+                                    DeptID = deptId
+                                };
+                                db.Set<Ticket>().Add(ticket);
+                                db.SaveChanges();
 
+                                // create ticket detail
+                                var listDetail = new List<TicketDetails>();
+                                foreach (var item in listTicketDetail)
+                                {
+                                    var ticketDetail = new TicketDetails
+                                    {
+                                        DateRequire = item.NgayCan,
+                                        Quantity = item.SoLuong,
+                                        Reason = item.LyDo,
+                                        Title = item.DienGiai,
+                                        TicketID = ticket.ID
+                                    };
+                                    listDetail.Add(ticketDetail);
+                                }
+                                db.Set<TicketDetails>().AddRange(listDetail);
+                                db.SaveChanges();
+
+                                // remove ticket detail session
+                                SessionUtilities.Set(SESSION.TicketDetail, null);
+                            }
+
+                        }
+                    }
+                }
                 return RedirectToAction("Index");
             }
             catch
