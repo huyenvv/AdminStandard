89feb473160fddb3751e7412012256035d1b7f19
bacsiden <duykhanh.sctn@gmail.com>
2015-07-30 10:14:45 -0700

    dfghj

--- Standard/Standard/Controllers/DeptController.cs
+++ Standard/Standard/Controllers/DeptController.cs
@@ -12,5 +12,43 @@ namespace Standard.Controllers
         {
             return View(db.Dept.ToList());
         }
-	}
+        public ActionResult Edit(int? id, string returnUrl)
+        {
+            Dept dept = new Dept();
+            if (id != null)
+                dept = db.Dept.FirstOrDefault(m => m.ID == id.Value);
+            return View(dept);
+        }
+        [HttpPost]
+        public ActionResult Edit(Dept model, string returnUrl)
+        {
+            if (model.ID == 0)
+            {
+                db.Dept.Add(model);
+            }
+            else
+                db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public string ListUser(int g)
+        {
+            var gr = new WebLib.Models.fwGroup() { ID = g };
+            string s = "<select name='LeaderUserID' class='form-control'><option value=''> -:- </option>";
+            foreach (var item in gr.fwUser)
+            {
+                s += string.Format("<option value='{0}'> {1} </option>", item.ID, item.Name);
+            }
+            return s + "</select>";
+        }
+        public ActionResult Delete(int id, string returnUrl)
+        {
+            var obj = db.Dept.FirstOrDefault(m => m.ID == id);
+            db.Dept.Remove(obj);
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
 }
