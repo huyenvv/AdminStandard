ff3bf894cfdfce1c3a63e5f1252580b4a294c2d5
huyenvv <huyenvv.it@gmail.com>
2015-08-01 22:37:21 +0700

    Add checkout Js

---
+++ Standard/Standard/Content/js/checkout.js
@@ -0,0 +1,21 @@
+﻿function CheckCheckout(flag) {
+    var check = true;
+    $("input, textarea", ".validationWrapper").each(function () {
+        var value = $(this).val();
+        if (value == "") {
+            check = false;
+            $(this).parents(".form-group").addClass("has-error");
+        } else {
+            $(this).parents(".form-group").removeClass("has-error");
+        }
+    });
+
+    if ($('input.onCheckbox:checked').length > 0) {
+        $("#loaiticket").addClass("hide");        
+    } else {
+        check = false;
+        $("#loaiticket").removeClass("hide");
+    }
+
+    return check;
+}
