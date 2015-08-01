c0094f141d62c89a10c5e1284c88b03ac715e901
huyenvv <huyenvv.it@gmail.com>
2015-08-02 00:25:39 +0700

    dddd

--- Standard/Standard/Content/js/ma-layout.js
+++ Standard/Standard/Content/js/ma-layout.js
@@ -4,4 +4,10 @@
         var ahref = $this.attr("href") + $this.attr("data-href");
         return ahref.indexOf("/" + getController() + "/") != -1 && ahref.indexOf("/" + getAction() + "/") != -1;
     }).addClass('active').parents("li").addClass('active').parents("ul").show().parents("li").addClass('toggled');
+
+    $(".Changpassword").click(function () {
+        var id = $(this).attr("href");
+        $(id).modal('show');
+    });
+    
 });
