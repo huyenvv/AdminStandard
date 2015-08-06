ae48999f79feb1ca469549f0b49649f9e17c5e73
bacsiden <duykhanh.sctn@gmail.com>
2015-08-06 09:43:33 -0700

    sdfghhjj

---
+++ Standard/Standard/Controllers/NghiController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Standard.Controllers
+{
+    public class NghiController : Controller
+    {
+        //
+        // GET: /Nghi/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
